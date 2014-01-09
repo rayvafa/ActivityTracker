23dde5cc734e0167bfa2aff08a5a007c646348eb
Ray Vafa <vafatabar@gmail.com>
2014-01-09 23:13:04 +1100

    test commit

--- scripts/coffeescripts/database/DataReader.coffee
+++ scripts/coffeescripts/database/DataReader.coffee
@@ -1,12 +1,14 @@
-#$.getJSON('data.php', function(data) {
-#$.each(data, function(fieldName, fieldValue) {
-#$("#" + fieldName).val(fieldValue);
-#});
-#});
 
 getActivities = () ->
   console.log 'getActivities'
   $.getJSON 'php/database/activities.php', (data) =>
+    data
+  .fail () ->
+      {}
+
+getTrackers = () ->
+  console.log 'getTrackers'
+  $.getJSON 'php/database/trackers.php', (data) =>
     data
   .fail () ->
       {}
--- scripts/javascripts/coffeebuilds/database/DataReader.js
+++ scripts/javascripts/coffeebuilds/database/DataReader.js
@@ -1,10 +1,20 @@
 (function() {
-  var getActivities;
+  var getActivities, getTrackers;
 
   getActivities = function() {
     var _this = this;
     console.log('getActivities');
     return $.getJSON('php/database/activities.php', function(data) {
+      return data;
+    }).fail(function() {
+      return {};
+    });
+  };
+
+  getTrackers = function() {
+    var _this = this;
+    console.log('getTrackers');
+    return $.getJSON('php/database/trackers.php', function(data) {
       return data;
     }).fail(function() {
       return {};
